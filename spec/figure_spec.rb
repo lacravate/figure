a5ef32aa84ecec144207e56f106f4d2487b6eeb5
lacravate <david.elbaz@af83.com>
2014-12-31 07:34:24 +0100

    Specs; organic code

---
+++ spec/figure_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe Figure do
+
+  describe "method missing" do
+    it "should otherwise respond to method_missing the usual way" do
+      expect{ described_class.plip }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "find config" do
+    it "should not parse a random file in config directory" do
+      expect{ described_class.other_config }.to raise_error(NoMethodError)
+    end
+
+    it "should find files to parse and get config from them" do
+      expect(described_class.plop.default.plop).to eq('plop')
+    end
+
+    it "should find Gaston files to parse and get config from them" do
+      expect(described_class.gaston.default.is_parsed).to eq(true)
+      expect(described_class.gaston.namespace.is_parsed).to eq('namespace')
+      expect(described_class.gaston.namespace.inherits_gaston_default).to eq(true)
+    end
+  end
+
+  describe 'defaults' do
+    it "should have the correct values for foo" do
+      expect(described_class.plop.foo.plop).to eq('plawp')
+      expect(described_class.plop.foo.plip).to eq('pleep')
+    end
+
+    it "should have the correct values for bar" do
+      expect(described_class.plop.bar.plop).to eq('plowp')
+      expect(described_class.plop.bar.plip).to eq(described_class.plop.default.plip)
+    end
+
+    it "should have the correct values for baz" do
+      expect(described_class.plop.baz.plop).to eq(described_class.plop.default.plop)
+      expect(described_class.plop.baz.plip).to eq(described_class.plop.default.plip)
+    end
+
+    it "should find defaults for much more nested values" do
+      expect(described_class.plop.default.more.nested.inherited.value).to eq('inherited')
+      expect(described_class.plop.much.more.nested.defined.value).to eq('value')
+      expect(described_class.plop.much.more.nested.inherited.value).to eq(described_class.plop.default.more.nested.inherited.value)
+    end
+  end
+
+  describe 'populate' do
+    it "should be possible to alter configuration on the fly" do
+      described_class.plop.much.more.nested.defined[:value] = "own_value"
+
+      expect(described_class.plop.much.more.nested.defined.value).to eq('own_value')
+    end
+
+    it "should be possible to add nested configuration on the fly" do
+      described_class.plop.much.more.nested.defined[:other_value] = "other_value"
+
+      expect(described_class.plop.much.more.nested.defined.other_value).to eq('other_value')
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+  config.profile_examples = 10
+  config.order = :random
+
+  # config.warnings = true
+  # config.default_formatter = 'doc' if config.files_to_run.one?
+
+  Kernel.srand config.seed
+end
+
+require_relative '../lib/figure'
+
+Figure.configure do |config|
+  config.config_directory = File.join File.dirname(__FILE__), 'fixtures'
+end
